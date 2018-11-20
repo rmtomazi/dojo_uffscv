c1a8a41c1f469dbd9cc6da7292cee57a80b76d08
aluno-uffs <Aluno UFFS>
2018-11-20 15:36:35 -0200

    HDR ok

--- HDR/HDR.py
+++ HDR/HDR.py
@@ -5,16 +5,64 @@ mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 
 #Pegar os tempos de abertura de câmera
-times = []
+times = np.array([1/30, 0.25, 2.5, 15.0], dtype = np.float32)
 
-filenames = []
+filenames = ["img_0.033.jpg", "img_0.25.jpg", "img_2.5.jpg", "img_15.jpg"]
 images = []
 hdr = []
 
 #Importando as Imagens
 for filename in filenames:
-    img = None
+    img = cv2.imread("images/" + filename)
     images.append(img)
+
+print("Alinhando a mediada do limite de bitmaps")
+alignMTB = cv2.createAlignMTB()
+alignMTB.process(images, images)
+
+print("Calibrando CRF")
+calibrateDebevec = cv2.createCalibrateDebevec()
+responseDevec = calibrateDebevec.process(images, times)
+
+print("Unindo as  Imagens")
+mergeDevec = cv2.createMergeDebevec()
+hdrDevec = mergeDevec.process(images, times, responseDevec
+)
+
+# Metodos de HDR
+# Drago
+print("Gerando HDR Drago")
+tonemapDrago = cv2.createTonemapDrago(1.0, 0.7)
+hdrDrago = tonemapDrago.process(hdrDevec)
+hdrDrago = 3 * hdrDrago
+hdrDrago = hdrDrago * 255
+
+hdr.append(hdrDrago)
+
+# Durand
+print("Gerando HDR Durand")
+tonemapDurand = cv2.createTonemapDurand(1.4, 4, 1.0, 1, 1)
+hdrDurand = tonemapDurand.process(hdrDevec)
+hdrDurand = 3 * hdrDurand
+hdrDurand = 255 * hdrDurand
+
+hdr.append(hdrDurand)
+
+# Reinhard
+print("Gerando HDR Reinhard")
+tonemapReinhard = cv2.createTonemapReinhard(1.5, 0, 0, 0)
+hdrReinhard = tonemapReinhard.process(hdrDevec)
+hdrReinhard = 255 * hdrReinhard
+
+hdr.append(hdrReinhard)
+
+# Mantiuk
+print("Gerando HDR Mantiuk")
+tonemapMantiuk = cv2.createTonemapMantiuk(2.2, 0.85, 1.2)
+hdrMantiuk = tonemapMantiuk.process(hdrDevec)
+hdrMantiuk = 255 * hdrMantiuk
+
+hdr.append(hdrMantiuk)
 
 fig=plt.figure(figsize=(10, 10))
 fig.canvas.set_window_title("Imagens Capturadas")
@@ -26,28 +74,9 @@ for i in range(1, columns*rows +1):
     plt.axis('off')
     plt.title("Exposição de {} segundos".format(times_labels[i-1]))
 
-    plt.imshow( )
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 
 plt.show()
-
-print("Alinhando a mediada do limite de bitmaps")
-
-print("Calibrando CRF")
-
-print("Unindo as  Imagens")
-
-# Metodos de HDR
-# Drago
-print("Gerando HDR Draco")
-
-# Durand
-print("Gerando HDR Durand")
-
-# Reinhard
-print("Gerando HDR Reinhard")
-
-# Mantiuk
-print("Gerando HDR Mantiuk")
 
 # Resultados Finais
 print("Gerando resultados Finais")
@@ -61,6 +90,6 @@ for i in range(1, columns*rows +1):
     plt.axis('off')
     plt.title(hdr_method[i-1])
 
-    plt.imshow( )
-    
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype(np.uint8))
+
 plt.show()
--- basico_opencv/basico.py
+++ basico_opencv/basico.py
@@ -2,32 +2,39 @@
 import cv2
 
 # Importar uma imagem
-img_COR =
-img_PB  =
+img_COR = cv2.imread("images/img.jpg")
+img_PB  = cv2.imread("images/img.jpg", 0)
 
 # Salvar uma imagem
-
+cv2.imwrite("images/saved.jpg", img_COR)
 
 # Plotar com OpenCV
+cv2.imshow("imagem", img_COR)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # Rotação
-linhas,colunas =
+linhas,colunas = img_PB.shape
 
-angulo =
+angulo = 90
 centro_de_rotacao = (colunas/2,linhas/2)
-escala =
+escala = 1
 
-rotacao =
-rotacionado =
+rotacao = cv2.getRotationMatrix2D(centro_de_rotacao, angulo, escala)
+rotacionado = cv2.warpAffine(img_PB, rotacao, (colunas, linhas))
 
+cv2.imshow("rotacionada", rotacionado);
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # Inter imagem vertical e horizontal
-inverte_horizontal =
-inverte_vertical =
+inverte_horizontal = cv2.flip(img_COR, 1)
+inverte_vertical = cv2.flip(img_PB, 0)
 
+cv2.imshow("horizontal", inverte_horizontal)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imshow("vertical", inverte_vertical)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
